edc0546cd434ab4040a53fcee7d276c1cc39247b
IgorEspinoza <igorespinoza10@gmail.com>
2022-12-14 21:09:39 -0300

    last version

--- assets/js/login.js
+++ assets/js/login.js
@@ -7,24 +7,24 @@ formulario.addEventListener('submit', function (e) {
 
   var data1 = user;
   var data2 = user + "/" + pas;
-
+  console.log(data1);
   const Datos = new FormData(formulario)
 
   async function postData(url = '', data = data1) {
     // Default options are marked with *
     const response = await fetch( url, {
-      method: 'GET', // *GET, POST, PUT, DELETE, etc.
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
       mode: 'cors', // no-cors, *cors, same-origin
       cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
       credentials: 'same-origin', // include, *same-origin, omit
       headers: {
-        //'Content-Type': 'application/json',
+        'Content-Type': 'application/json',
         //'Content-Type': 'application/x-www-form-urlencoded', 
         
       },
       redirect: 'follow', // manual, *follow, error
       referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      //body: data // body data type must match "Content-Type" header
+      body: JSON.stringify(data) // body data type must match "Content-Type" header
     });
     return response.json(); // parses JSON response inzto native JavaScript objects
   }
